603b20df65cdd15a496c92a8c4a03dffd50df122
Alexander <a.pride.xxi@tutanota.com>
2022-08-13 15:22:54 +0300

    AppCoordinator

---
+++ Event Club/AppCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  AppCoordinator.swift
+//  Event Club
+//
+//  Created by Александр Прайд on 13.08.2022.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinator: [Coordinator] { get }
+    func start()
+}
+
+final class AppCoordinator: Coordinator {
+    private(set) var childCoordinator: [Coordinator] = []
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+     
+}
