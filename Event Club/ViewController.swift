b413c58851adb0f398b82232da6a40ca05c3a446
Alexander <a.pride.xxi@tutanota.com>
2022-08-13 14:09:55 +0300

    initial commit

---
+++ Event Club/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Club
+//
+//  Created by Александр Прайд on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
