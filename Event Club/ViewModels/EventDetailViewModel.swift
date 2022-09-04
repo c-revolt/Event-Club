154e0b263070218b948a260d14cc5fe0d9006a9f
Alexander <a.pride.xxi@tutanota.com>
2022-09-04 11:49:03 +0300

    EventDetailViewModel

---
+++ Event Club/ViewModels/EventDetailViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  EventDetailViewModel.swift
+//  Event Club
+//
+//  Created by Александр Прайд on 04.09.2022.
+//
+
+import UIKit
+import CoreData
+
+final class EventDetailViewModel {
+    
+    private let eventID: NSManagedObjectID
+    private let coreDataManager: CoreDataManager
+    private var event: Event?
+    var onUpdate = {}
+    
+    var image: UIImage? {
+        guard let imageData = event?.image else { return nil }
+        return UIImage(data: imageData)
+        
+    }
+    
+    init(eventID: NSManagedObjectID, coreDataManager: CoreDataManager = .shared) {
+        self.eventID = eventID
+        self.coreDataManager = coreDataManager
+    }
+    
+    func viewDidLoad() {
+        event = coreDataManager.getEvent(eventID)
+        onUpdate()
+    }
+    
+}
